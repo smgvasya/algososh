199abde0c26ac92e340de7e222a60fbfdac455a1
smgvasya <deepxp@yandex.ru>
2023-07-22 02:48:39 +0300

    string-form

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,10 +1,29 @@
 import React from "react";
+import styles from "./fibonacci-page.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
 
 export const FibonacciPage: React.FC = () => {
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
-     
+      <form className={styles.form}>
+        <Input
+          placeholder = "Введите число"
+          max={19}
+          isLimitText={true}
+          //value={}
+          //onChange={}
+          type="number"
+        />
+        <Button
+          text="Развернуть"
+          type="submit"
+          //isLoader={isLoading}
+          disabled
+        />
+      </form>
     </SolutionLayout>
   );
 };
--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,32 @@
-import React from "react";
+import {useState}from "react";
+import styles from "./string.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
 
 export const StringComponent: React.FC = () => {
+
+  // const [string, setString] = useState<string>('');
+
+
+  
   return (
     <SolutionLayout title="Строка">
-     
+      <form className={styles.form}>
+        <Input
+          maxLength={11}
+          isLimitText={true}
+          //value={}
+          //onChange={}
+        />
+        <Button
+          text="Развернуть"
+          type="submit"
+          //isLoader={isLoading}
+          disabled
+        />
+      </form>
     </SolutionLayout>
   );
 };
