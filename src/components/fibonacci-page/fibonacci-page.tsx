aa3d4624971ab14cb758c8d7df3334aafefaa941
smgvasya <deepxp@yandex.ru>
2023-07-27 19:28:46 +0300

    fibonacci-Done

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -4,11 +4,8 @@ import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
-import { DELAY_IN_MS, SHORT_DELAY_IN_MS } from "../../utils/constants/delays";
-import { ElementStates } from "../../types/element-states";
-import { StrReversType } from "../../types/types";
-import { swap, delay } from "../../utils/index";
-
+import { SHORT_DELAY_IN_MS } from "../../utils/constants/delays";
+import { delay } from "../../utils/index";
 
 export const FibonacciPage: React.FC = () => {
   const [value, setValue] = useState<number>();
@@ -19,12 +16,41 @@ export const FibonacciPage: React.FC = () => {
     setValue(Number(evt.target.value));
   };
 
-  
+  const fibonacciCalc = (index: number): number => {
+    if (index < 0) {
+      throw new Error("число не может быть меньше нуля");
+    }
+    if (index === 0) {
+      return 0;
+    }
+    if (index === 1) {
+      return 1;
+    }
+    return fibonacciCalc(index - 1) + fibonacciCalc(index - 2);
+  };
+
+  const fibonacciRender = async (index: number) => {
+    setIsLoading(true);
+    const arr: number[] = [];
+    for (let i = 1; i <= index + 1; i++) {
+      await delay(SHORT_DELAY_IN_MS);
+      arr.push(fibonacciCalc(i));
+      setFibonacci([...arr]);
+    }
+    setIsLoading(false);
+  };
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    fibonacciRender(value as number);
+    setValue(0);
+  };
+
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <Input
-          placeholder = "Введите число"
+          placeholder="Введите число"
           max={19}
           isLimitText={true}
           value={value}
@@ -38,6 +64,15 @@ export const FibonacciPage: React.FC = () => {
           disabled={!value}
         />
       </form>
+      {fibonacci && (
+        <ul className={styles.container}>
+          {fibonacci.map((item, index) => (
+            <li className={styles.circles} key={index}>
+              <Circle letter={item} index={index}></Circle>
+            </li>
+          ))}
+        </ul>
+      )}
     </SolutionLayout>
   );
 };
